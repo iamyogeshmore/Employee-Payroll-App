00382f96cc6f4964694259c7775b99956241fad9
Yogesh More <yogesh.more2098@gmail.com>
2023-06-13 11:47:02 +0530

    Added:  Event listener on document load to set event listener for the 'name' field.

--- employee payroll form.js
+++ employee payroll form.js
@@ -6,3 +6,22 @@ output.textContent = salary.value;
 salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
+
+//  <!--Uc 12 – On Document Load Set Event Listeners.-->
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const nameError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      nameError.textContent = "";
+      return;
+    }
+    try {
+      new EmployeePayrollData().name = name.value;
+      nameError.textContent = "";
+    } catch (e) {
+      nameError.textContent = e;
+    }
+  });
+});
